9b95d40e3a5e264ca619930ca6ddb8b65aa43951
hlaclau <hugo.laclau@gmail.com>
2026-03-24 10:12:49 +0100

    feat(task 1): seed user data

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+export const users = [
+  {
+    id: 1,
+    name: "Nicolas LeTigre",
+    email: "tieun@tigre.com",
+    role: "admin",
+    createdAt: "2025-03-24"
+  },
+  {
+    id: 2,
+    name: "Tommy Dumoumou",
+    email: "tommyditleking@gmail.com",
+    role: "user",
+    createdAt: "2025-03-24"
+  },
+  {
+    id: 3,
+    name: "Simon Dugoat",
+    email: "joueurdechecquibatdesenfants@gmail.com",
+    role: "user",
+    createdAt: "2025-03-24"
+  }
+];
