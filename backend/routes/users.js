3ccb41b9523aeaa68e5aa78358d0970c6bc8d44c
hlaclau <hugo.laclau@gmail.com>
2026-03-24 10:14:42 +0100

    feat(task 2.1): implement a route to find all the users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import { users } from '../data/users.js';
+
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    count: users.length,
+    data: users,
+  });
+});
 
 export default router;
