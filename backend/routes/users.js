29bd2d416e70a1c9bfb3652fb5053b2b5f2d1334
hlaclau <hugo.laclau@gmail.com>
2026-03-24 10:18:19 +0100

    feat(task 2.2): implement a route to find a user by its id

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,4 +11,20 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: 'Utilisateur non trouvé',
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 export default router;
