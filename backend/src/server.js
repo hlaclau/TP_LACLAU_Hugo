2b9a7fdc884139857ce92aed30d3c02e24b01f89
hlaclau <hugo.laclau@gmail.com>
2026-03-27 12:11:23 +0100

    feat(task bonus c): implement a middleware to handler errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
 import logger from "./middlewares/logger.js";
+import errorHandler from "./middlewares/errorHandler.js";
 import usersRouter from "./routes/users.js";
 
 await connectDB();
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(logger);
 
 app.use("/api/users", usersRouter);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
